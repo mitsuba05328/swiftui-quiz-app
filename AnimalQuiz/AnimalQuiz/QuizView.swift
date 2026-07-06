ce9eb22b560f78a81db3dcd8e58639b41a4cd21e
haruki328 <y.haruki328@gmail.com>
2026-07-06 11:13:25 +0900

    update 問題番号

--- AnimalQuiz/AnimalQuiz/QuizView.swift
+++ AnimalQuiz/AnimalQuiz/QuizView.swift
@@ -9,28 +9,47 @@ import SwiftUI
 
 struct QuizView: View {
     @State var isShowingScoreView = false
-    
+    let choices = ["ライオン","ウサイン・ボルト","チーター","馬",]
     var body: some View {
         VStack {
-            Text("問題番号：1/10")
-            Text("次のうち、世界で最も早く走る動物はどれですか？")
+            Text("問題番号：1/5")
+                .font(.headline)
+                .padding(10)
+                .background(Color.originalGreen)
+                .foregroundStyle(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
             
-            Button("ライオン") {
-                isShowingScoreView = true
-            }
-            .fullScreenCover(isPresented: $isShowingScoreView) {
-                ScoreView()
-            }
-            Button("ウサイン・ボルト") {
-            }
-            Button("チーター") {
-            }
-            Button("馬") {
+            Text("次のうち、世界で最も早く走る動物はどれですか？")
+                .font(.title)
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Color.originalLightGreen)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(.originalGreen, lineWidth: 5)
+                )
+                .frame(maxHeight: .infinity)
+            ForEach(choices, id: \.self) { choice in
+                Button {
+                    isShowingScoreView = true
+                } label: {
+                    Text(choice)
+                        .font(.title)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.originalSkin)
+                        .foregroundStyle(.originalBrown)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                }
+                .fullScreenCover(isPresented: $isShowingScoreView) {
+                    ScoreView()
+                }
             }
         }
+        .padding()
+        .backgroundImage()
     }
 }
-
 #Preview {
     QuizView()
 }
